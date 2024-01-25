75a006e61872085889a089a43aae54c4e2778899
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-25 15:26:11 -0500

    dev'd front end mutations and queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Include:
+// ADD_USER
+// SAVE_BOOK
+// REMOVE_BOOK
