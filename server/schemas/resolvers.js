e1939c1fb7124d5bba371f0defc2fbab36a23061
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-25 15:00:04 -0500

    implemented apollo server and applied to express as middleware

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,4 @@
+// Activity 21
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
--- server/server.js
+++ server/server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+// Import the ApolloServer Class
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// Create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +26,38 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
