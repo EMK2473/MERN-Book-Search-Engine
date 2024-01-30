f9286ebc9b3918356a9849d821d770628d5a021e
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-30 16:46:28 -0500

    auth.js changes

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,46 +1,37 @@
 import decode from 'jwt-decode';
 
-// create a new class to instantiate for a user
 class AuthService {
-  // get user data
   getProfile() {
     return decode(this.getToken());
   }
 
-  // check if user's logged in
   loggedIn() {
-    // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
+    return !!token && !this.isTokenExpired(token);
   }
 
-  // check if token is expired
   isTokenExpired(token) {
     try {
       const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
+      return decoded.exp < Date.now() / 1000;
     } catch (err) {
       return false;
     }
   }
 
   getToken() {
-    // Retrieve user token from localStorage
     return localStorage.getItem('id_token');
   }
 
   login(idToken) {
-    // Save user token to localStorage
     localStorage.setItem('id_token', idToken);
+    // Redirect to the home page or the intended destination
     window.location.assign('/');
   }
 
   logout() {
-    // Clear user token and profile from localStorage
     localStorage.removeItem('id_token');
-    // reload the page and reset the state of app
+    // Redirect to the home page or the login page
     window.location.assign('/');
   }
 }
