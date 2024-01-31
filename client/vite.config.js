36b36f4936e375d9d21d878d09a8c86b3d256e0c
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-31 18:21:02 -0500

    added catch all route

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       '/graphQL': {
+//         target: 'http://localhost:3001',
+//         secure: false,
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+     plugins: [react()],
+     build: {
+          base: "./", // Add this line
+          outDir: "dist", // Output directory for the build.
+          emptyOutDir: true, // Empty the output directory when the build starts.
+     },
+     server: {
+          port: 3000,
+          open: true,
+          proxy: {
+               "/graphql": {
+                    target: "http://localhost:3001",
+                    secure: false,
+                    changeOrigin: true,
+               },
+          },
+     },
+});
--- server/server.js
+++ server/server.js
@@ -23,7 +23,10 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
 
 
@@ -42,15 +45,3 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
-
-
-
-
-
-
-
-
-
-
-
-
